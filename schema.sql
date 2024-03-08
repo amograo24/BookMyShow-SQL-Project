aaffc7733c2851b1e797777ef06139942063e34b
nimratkk <nimrat.caler@gmail.com>
2024-03-09 02:37:48 +0530

    Created theatres and audis tables

--- schema.sql
+++ schema.sql
@@ -2,10 +2,17 @@ CREATE TABLE IF NOT EXISTS movies(
 
 );
 CREATE TABLE IF NOT EXISTS theatres(
-
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    PRIMARY KEY("id")
 );
 CREATE TABLE IF NOT EXISTS audis(
-
+    "id" INTEGER,
+    "name" TEXT,
+    "theatre_id" INTEGER NOT NULL,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("theatre_id") REFERENCES "theatres"("id")
 );
 CREATE TABLE IF NOT EXISTS seats(
 
